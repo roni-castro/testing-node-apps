100eb7f932e899418627067d29261381b2b440db
Roni <roni.c.castro@gmail.com>
2022-06-05 19:29:36 -0300

    test: missing functions time calls

--- src/utils/__tests__/error-middleware.exercise.js
+++ src/utils/__tests__/error-middleware.exercise.js
@@ -4,26 +4,28 @@ import {UnauthorizedError} from 'express-jwt'
 import {buildNext, buildReq, buildRes} from 'utils/generate'
 import errorMiddleware from '../error-middleware'
 
-test('UnauthorizedError is returned with error code and message', () => {
+test('UnauthorizedError is returned with error code 401 and error message', () => {
   const error = new UnauthorizedError('some_error_code', {
     message: 'Some message',
   })
   const req = buildReq()
   const res = buildRes()
+  const next = buildNext()
 
   errorMiddleware(error, req, res)
 
   expect(res.status).toHaveBeenCalledWith(401)
+  expect(res.status).toHaveBeenCalledTimes(1)
   expect(res.json).toHaveBeenCalledWith({
     code: error.code,
     message: error.message,
   })
+  expect(res.json).toHaveBeenCalledTimes(1)
+  expect(next).not.toHaveBeenCalled()
 })
 
-test('when headersSent is true it return the error and no new response', () => {
-  const error = new UnauthorizedError('some_error_code', {
-    message: 'Some message',
-  })
+test('calls next if headersSent is true', () => {
+  const error = new Error('Some message')
   const res = buildRes({headersSent: true})
   const req = buildReq()
   const next = buildNext()
@@ -31,18 +33,25 @@ test('when headersSent is true it return the error and no new response', () => {
   errorMiddleware(error, req, res, next)
 
   expect(next).toHaveBeenCalledWith(error)
+  expect(next).toHaveBeenCalledTimes(1)
+  expect(res.json).not.toHaveBeenCalled()
+  expect(res.status).not.toHaveBeenCalled()
 })
 
 test('error 500 is return with stack trace and message', () => {
   const error = new Error('some message')
   const res = buildRes()
   const req = buildReq()
+  const next = buildNext()
 
   errorMiddleware(error, req, res)
 
   expect(res.status).toHaveBeenCalledWith(500)
+  expect(res.status).toHaveBeenCalledTimes(1)
   expect(res.json).toHaveBeenCalledWith({
     message: error.message,
     stack: error.stack,
   })
+  expect(res.json).toHaveBeenCalledTimes(1)
+  expect(next).not.toHaveBeenCalled()
 })
