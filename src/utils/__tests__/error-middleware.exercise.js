73b5439e8e2b3e83790145f7a0b72a5be15e7f6d
Roni <roni.c.castro@gmail.com>
2022-06-05 18:20:03 -0300

    test: error-middleware

--- src/utils/__tests__/error-middleware.exercise.js
+++ src/utils/__tests__/error-middleware.exercise.js
@@ -1,16 +1,46 @@
 // Testing Middleware
 
-// 💣 remove this todo test (it's only here so you don't get an error about missing tests)
-test.todo('remove me')
+import {UnauthorizedError} from 'express-jwt'
+import errorMiddleware from '../error-middleware'
 
-// 🐨 you'll need both of these:
-// import {UnauthorizedError} from 'express-jwt'
-// import errorMiddleware from '../error-middleware'
+test('UnauthorizedError is returned with error code and message', () => {
+  const error = new UnauthorizedError('some_error_code', {
+    message: 'Some message',
+  })
+  const res = {json: jest.fn(() => res), status: jest.fn(() => res)}
 
-// 🐨 Write a test for the UnauthorizedError case
-// 💰 const error = new UnauthorizedError('some_error_code', {message: 'Some message'})
-// 💰 const res = {json: jest.fn(() => res), status: jest.fn(() => res)}
+  errorMiddleware(error, {}, res)
 
-// 🐨 Write a test for the headersSent case
+  expect(res.status).toHaveBeenCalledWith(401)
+  expect(res.json).toHaveBeenCalledWith({
+    code: error.code,
+    message: error.message,
+  })
+})
 
-// 🐨 Write a test for the else case (responds with a 500)
+test('when headerSent is true it return the error', () => {
+  const error = new UnauthorizedError('some_error_code', {
+    message: 'Some message',
+  })
+  const res = {headersSent: true}
+  const next = jest.fn()
+
+  errorMiddleware(error, {}, res, next)
+
+  expect(next).toHaveBeenCalledWith(error)
+})
+
+test('error 500 is return with stack trace and message', () => {
+  const error = new Error({
+    message: 'Some message',
+  })
+  const res = {json: jest.fn(() => res), status: jest.fn(() => res)}
+
+  errorMiddleware(error, {}, res)
+
+  expect(res.status).toHaveBeenCalledWith(500)
+  expect(res.json).toHaveBeenCalledWith({
+    message: error.message,
+    stack: error.stack,
+  })
+})
