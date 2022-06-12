35ca4c304514f5ad791d3b103d1238a61c74746a
Roni <roni.c.castro@gmail.com>
2022-06-12 17:52:42 -0300

    test: auth login and  me requests

--- src/__tests__/auth.exercise.js
+++ src/__tests__/auth.exercise.js
@@ -2,8 +2,9 @@
 
 import axios from 'axios'
 import {resetDb} from 'utils/db-utils'
-import {getData, handleRequestFailure} from 'utils/async'
+import {getData, handleRequestFailure, resolve} from 'utils/async'
 import * as generate from 'utils/generate'
+import passport from 'passport'
 import * as usersDB from '../db/users'
 import startServer from '../start'
 
@@ -62,7 +63,7 @@ describe('#/auth/register', () => {
     )
   })
 
-  test('fails with error 400 when a unsername is not passed', async () => {
+  test('fails with error 400 when a username is not passed', async () => {
     const userCredentials = generate.loginForm({username: null})
     await expect(
       client.post('/auth/register', userCredentials),
@@ -86,6 +87,70 @@ describe('#/auth/register', () => {
       client.post('/auth/register', userCredentials),
     ).rejects.toMatchInlineSnapshot(
       `[Error: 400: {"message":"password is not strong enough"}]`,
+    )
+  })
+})
+
+describe('#/auth/login', () => {
+  test('fails with error 400 when a username is not passed', async () => {
+    const userCredentials = generate.loginForm({username: null})
+    await expect(
+      client.post('/auth/login', userCredentials),
+    ).rejects.toMatchInlineSnapshot(
+      `[Error: 400: {"message":"username can't be blank"}]`,
+    )
+  })
+
+  test('fails with error 400 when password is not passed', async () => {
+    const userCredentials = generate.loginForm({password: null})
+    await expect(
+      client.post('/auth/login', userCredentials),
+    ).rejects.toMatchInlineSnapshot(
+      `[Error: 400: {"message":"password can't be blank"}]`,
+    )
+  })
+
+  test('fails with error 400 when user credentials are invalid', async () => {
+    const userCredentials = generate.loginForm({username: 'invalid'})
+    await expect(
+      client.post('/auth/login', userCredentials),
+    ).rejects.toMatchInlineSnapshot(
+      `[Error: 400: {"message":"username or password is invalid"}]`,
+    )
+  })
+
+  test('fails with custom error when passport authenticate fails', async () => {
+    const errorMessage = 'passport error'
+    jest
+      .spyOn(passport, 'authenticate')
+      .mockImplementation((_, __, callback) =>
+        callback(new Error(errorMessage)),
+      )
+    const userCredentials = generate.loginForm({username: 'invalid'})
+    const error = await client
+      .post('/auth/login', userCredentials)
+      .catch(resolve)
+
+    expect(error.status).toEqual(500)
+    expect(error.message).toMatch(new RegExp(`${errorMessage}`))
+  })
+})
+
+describe('#/me', () => {
+  test('fails with 401 error when Authorization header is not provided', async () => {
+    await expect(client.get('/auth/me')).rejects.toMatchInlineSnapshot(
+      `[Error: 401: {"code":"credentials_required","message":"No authorization token was found"}]`,
+    )
+  })
+
+  test('fails with 401 error when token is invalid', async () => {
+    const token = 'null'
+    await expect(
+      client.get('/auth/me', {
+        headers: {Authorization: `Bearer ${token}`},
+      }),
+    ).rejects.toMatchInlineSnapshot(
+      `[Error: 401: {"code":"invalid_token","message":"jwt malformed"}]`,
     )
   })
 })
